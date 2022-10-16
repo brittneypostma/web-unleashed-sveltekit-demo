bf51c97dbda006518ae5fb15e6fa6e4404bc4e71
brittneypostma <bdesignedwebdev@gmail.com>
2022-10-16 12:44:05 -0400

    added tailwind and changed bg color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script>
+	import '../app.postcss';
 	import Header from './Header.svelte';
 	import './styles.css';
 </script>
 
-<div class="app">
+<div class="app bg-teal-400">
 	<Header />
 
 	<main>
